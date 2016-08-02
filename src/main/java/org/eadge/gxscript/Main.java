dcfc2aa2df13f25a71b4a39ace066d8411d9ae43
eadgyo <eadgyo@gmail.com>
2016-08-02 16:01:08 +0200

    Init java project

---
+++ src/main/java/org/eadge/gxscript/Main.java
@@ -0,0 +1,15 @@
+package org.eadge.gxscript;
+
+import java.io.IOException;
+
+/**
+ * Created by eadgyo on 02/08/16.
+ *
+ */
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+
+    }
+}
