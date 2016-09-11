a33f7cef1e022cb3edd616a80faf2560dd6b9c9a
eadgyo <eadgyo@gmail.com>
2016-09-11 13:19:56 +0200

    End test while.

--- src/main/java/org/eadge/gxscript/classic/test/imbrication/TestWhile.java
+++ src/main/java/org/eadge/gxscript/classic/test/imbrication/TestWhile.java
@@ -3,6 +3,7 @@ package org.eadge.gxscript.classic.test.imbrication;
 import org.eadge.gxscript.classic.entity.displayer.PrintEntity;
 import org.eadge.gxscript.classic.entity.imbrication.loops.WhileEntity;
 import org.eadge.gxscript.classic.entity.types.bool.BoolEntity;
+import org.eadge.gxscript.classic.entity.types.bool.ModifyBoolEntity;
 import org.eadge.gxscript.classic.entity.types.bool.transform.InvertBoolEntity;
 import org.eadge.gxscript.classic.entity.types.number.IntEntity;
 import org.eadge.gxscript.classic.entity.types.number.comparison.EqualToNumberEntity;
@@ -22,7 +23,7 @@ public class TestWhile
 {
     public static void main(String[] args) throws IOException
     {
-        System.out.println("Test switch");
+        System.out.println("Test while");
 
         PrintTest.printResult(test(), "Check valid script");;
     }
@@ -47,9 +48,11 @@ public class TestWhile
 
         // Invert result
         InvertBoolEntity invertBoolEntity = new InvertBoolEntity();
+        InvertBoolEntity invertBoolEntity1 = new InvertBoolEntity();
 
         // Save the result
         BoolEntity boolEntity = new BoolEntity();
+        ModifyBoolEntity modifyBoolEntity = new ModifyBoolEntity();
 
         // Create modifiying elements
         ModifyingEntity modificationEntity = intEntity.createModificationEntity(IntEntity.INT_OUTPUT_INDEX);
@@ -65,10 +68,6 @@ public class TestWhile
         equalToNumberEntity.linkAsInput(EqualToNumberEntity.V0_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, intEntity);
         equalToNumberEntity.linkAsInput(EqualToNumberEntity.V1_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, intEntity1);
 
-        equalToNumberEntity1.linkAsInput(EqualToNumberEntity.V0_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, intEntity);
-        equalToNumberEntity1.linkAsInput(EqualToNumberEntity.V1_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, intEntity1);
-        equalToNumberEntity1.linkAsInput(EqualToNumberEntity.NEXT_INPUT_INDEX, 0, modificationEntity);
-
         // Invert the result
         invertBoolEntity.linkAsInput(InvertBoolEntity.SOURCE_INPUT_INDEX, EqualToNumberEntity.RESULT_OUTPUT_INDEX, equalToNumberEntity);
 
@@ -78,7 +77,6 @@ public class TestWhile
         // Link result to while
         whileEntity.linkAsInput(WhileEntity.TEST_INPUT_INDEX, BoolEntity.BOOL_OUTPUT_INDEX, boolEntity);
 
-
         // WHILE PROCESS
         // Link print to while do
         printEntity.linkAsInput(PrintEntity.NEXT_INPUT_INDEX, WhileEntity.DO_OUTPUT_INDEX, whileEntity);
@@ -86,10 +84,23 @@ public class TestWhile
         // Then remove one from the int 0
         addNumbersEntity.linkAsInput(AddNumbersEntity.S0_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, intEntity);
         addNumbersEntity.linkAsInput(AddNumbersEntity.S1_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, addEntity);
+        addNumbersEntity.linkAsInput(AddNumbersEntity.NEXT_INPUT_INDEX, PrintEntity.CONTINUE_OUTPUT_INDEX, printEntity);
 
-        // Modify the boolean
+        // Modify int
         modificationEntity.linkAsInput(0, IntEntity.INT_OUTPUT_INDEX, intEntity);
         modificationEntity.linkAsInput(1, AddNumbersEntity.RESULT_OUTPUT_INDEX, addNumbersEntity);
+
+        // Test conditions
+        equalToNumberEntity1.linkAsInput(EqualToNumberEntity.V0_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, intEntity);
+        equalToNumberEntity1.linkAsInput(EqualToNumberEntity.V1_INPUT_INDEX, IntEntity.INT_OUTPUT_INDEX, intEntity1);
+        equalToNumberEntity1.linkAsInput(EqualToNumberEntity.NEXT_INPUT_INDEX, 0, modificationEntity);
+
+        // Invert the result
+        invertBoolEntity1.linkAsInput(InvertBoolEntity.SOURCE_INPUT_INDEX, EqualToNumberEntity.RESULT_OUTPUT_INDEX, equalToNumberEntity1);
+
+        // Modify the boolean
+        modifyBoolEntity.linkAsInput(ModifyBoolEntity.MODIFIED_INPUT_INDEX, BoolEntity.BOOL_OUTPUT_INDEX, boolEntity);
+        modifyBoolEntity.linkAsInput(ModifyBoolEntity.SOURCE_INPUT_INDEX, InvertBoolEntity.INVERTED_OUTPUT_INDEX, invertBoolEntity1);
 
         rawGXScript.addEntityRecursiveSearch(intEntity);
         return RunTest.run(rawGXScript);
