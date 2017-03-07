46a563fd0ef8e532d76917dcae90c444c81a050f
eadgyo <eadgyo@gmail.com>
2017-03-07 16:21:57 +0100

    [UPDATE] Change EGXGroup

--- src/main/java/org/eadge/gxscript/data/io/EGXGroup.java
+++ src/main/java/org/eadge/gxscript/data/io/EGXGroup.java
@@ -2,23 +2,17 @@ package org.eadge.gxscript.data.io;
 
 import org.eadge.gxscript.data.entity.model.base.GXEntity;
 
-import java.io.Serializable;
-import java.util.Collection;
+import java.util.Vector;
 
 /**
  * Created by eadgyo on 05/03/17.
  *
  * Contain GXElements, associated with this group
  */
-public class EGXGroup implements Serializable
+public class EGXGroup extends Vector<GXEntity>
 {
     /**
      * Keeps the name of the GXGroup
      */
     private String name;
-
-    /**
-     * Holds all elements
-     */
-    private Collection<? extends GXEntity> elements;
 }
