d9d90f922ced8201fc8a90c690136800f0024e6b
eadgyo <eadgyo@gmail.com>
2017-03-07 16:23:51 +0100

    [UPDATE] Change EGX

--- src/main/java/org/eadge/gxscript/data/io/EGX.java
+++ src/main/java/org/eadge/gxscript/data/io/EGX.java
@@ -1,21 +1,13 @@
 package org.eadge.gxscript.data.io;
 
 
-import java.io.Serializable;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by eadgyo on 05/03/17.
  *
  * Used to save GXElements
  */
-public class EGX implements Serializable
+public class EGX extends HashMap<String, EGXGroup>
 {
-    private Map<String, EGXGroup> egxGroups;
-
-    public EGX()
-    {
-        new HashMap<>();
-    }
 }
